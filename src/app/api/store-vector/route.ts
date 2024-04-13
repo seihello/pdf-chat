5a8878ea8256f94965d4dbc1d63293d79583a707
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-04-13 01:44:11 -0700

    improvement: catch error when generating vectors

--- src/app/api/store-vector/route.ts
+++ src/app/api/store-vector/route.ts
@@ -11,41 +11,48 @@ import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { session_id, file_url } = await req.json();
-  const response = await fetch(file_url);
-  const blob = await response.blob();
+  try {
+    const { session_id, file_url } = await req.json();
+    const response = await fetch(file_url);
+    const blob = await response.blob();
 
-  // Request the OpenAI API for the response based on the prompt
-  const loader = new PDFLoader(blob, {
-    splitPages: false,
-  });
+    // Request the OpenAI API for the response based on the prompt
+    const loader = new PDFLoader(blob, {
+      splitPages: false,
+    });
 
-  const docs = await loader.load();
-  const textSplitter = new CharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  });
-  const docsWithMetadata = docs.map((doc) => {
-    return { ...doc, metadata: { ...doc.metadata, session_id: session_id } };
-  });
+    const docs = await loader.load();
+    const textSplitter = new CharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+    const docsWithMetadata = docs.map((doc) => {
+      return { ...doc, metadata: { ...doc.metadata, session_id: session_id } };
+    });
 
-  const splitDocs = await textSplitter.splitDocuments(docsWithMetadata);
-  console.log({ splitDocs });
+    const splitDocs = await textSplitter.splitDocuments(docsWithMetadata);
+    console.log({ splitDocs });
 
-  const supabase = await createClient();
-  await SupabaseVectorStore.fromDocuments(
-    splitDocs,
-    new OpenAIEmbeddings({
-      openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-    }),
-    {
-      client: supabase,
-      tableName: "documents",
-    },
-  );
+    const supabase = await createClient();
+    await SupabaseVectorStore.fromDocuments(
+      splitDocs,
+      new OpenAIEmbeddings({
+        openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+      }),
+      {
+        client: supabase,
+        tableName: "documents",
+      },
+    );
 
-  return new Response(null, {
-    status: 200,
-  });
+    return new Response(null, {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(String(error), {
+      status: 200,
+    });
+  }
   // return new StreamingTextResponse(stream);
 }
