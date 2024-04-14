47d005166803d291953b2aab2b9b49ce9ef8070b
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-04-13 17:24:02 -0700

    fix: add console lo to inspect the issue

--- src/app/api/store-vector/route.ts
+++ src/app/api/store-vector/route.ts
@@ -11,29 +11,47 @@ import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
+  console.log("store-vector");
+
   try {
     const { session_id, file_url } = await req.json();
+    console.log("aaa");
+
     const response = await fetch(file_url);
+    console.log("bbb");
+
     const blob = await response.blob();
+    console.log("ccc");
 
     // Request the OpenAI API for the response based on the prompt
     const loader = new PDFLoader(blob, {
       splitPages: false,
     });
 
+    console.log("ddd");
+
     const docs = await loader.load();
+    console.log("eee");
+
     const textSplitter = new CharacterTextSplitter({
       chunkSize: 1000,
       chunkOverlap: 200,
     });
+    console.log("fff");
+
     const docsWithMetadata = docs.map((doc) => {
       return { ...doc, metadata: { ...doc.metadata, session_id: session_id } };
     });
 
+    console.log("ggg");
+
     const splitDocs = await textSplitter.splitDocuments(docsWithMetadata);
     console.log({ splitDocs });
+    console.log("hhh");
 
     const supabase = await createClient();
+    console.log("iii");
+
     await SupabaseVectorStore.fromDocuments(
       splitDocs,
       new OpenAIEmbeddings({
@@ -44,6 +62,7 @@ export async function POST(req: Request) {
         tableName: "documents",
       },
     );
+    console.log("jjj");
 
     return new Response(null, {
       status: 200,
